d40bd927dcc6a47db229e4ada4fc1aba2e723e6a
student <student@PC-629.urel.feec.vutbr.cz>
2022-03-08 16:45:41 +0100

    Update lab 4

--- labs/04-segment/display/display.srcs/sim_1/new/tb_hex_7seg.vhd
+++ labs/04-segment/display/display.srcs/sim_1/new/tb_hex_7seg.vhd
@@ -1,36 +1,46 @@
-# Lab 3: Petra Slotova
+------------------------------------------------------------
+--
+-- Testbench for 7-segment display decoder.
+-- Nexys A7-50T, Vivado v2020.1, EDA Playground
+--
+-- Copyright (c) 2020-Present Tomas Fryza
+-- Dept. of Radio Electronics, Brno Univ. of Technology, Czechia
+-- This work is licensed under the terms of the MIT license.
+--
+------------------------------------------------------------
 
-## 1. Preparation
+library ieee;
+use ieee.std_logic_1164.all;
 
-| **Hex** | **Inputs** | **A** | **B** | **C** | **D** | **E** | **F** | **G** |
-   | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: |
-   | 0 | 0000 | 0 | 0 | 0 | 0 | 0 | 0 | 1 |
-   | 1 | 0001 | 1 | 0 | 0 | 1 | 1 | 1 | 1 |
-   | 2 | 0010 | 0 | 0 | 1 | 0 | 0 | 1 | 0 |
-   | 3 | 0011 | 0 | 0 | 0 | 0 | 1 | 1 | 0 |
-   | 4 | 0100 | 1 | 0 | 0 | 1 | 1 | 0 | 0 |
-   | 5 | 0101 | 0 | 1 | 0 | 0 | 1 | 0 | 0 |
-   | 6 | 0110 | 0 | 1 | 0 | 0 | 0 | 0 | 0 |
-   | 7 | 0111 | 0 | 0 | 0 | 1 | 1 | 1 | 1 |
-   | 8 | 1000 | 0 | 0 | 0 | 0 | 0 | 0 | 0 |
-   | 9 | 1001 | 0 | 0 | 0 | 0 | 1 | 0 | 0 |
-   | A | 1010 | 0 | 0 | 0 | 1 | 0 | 0 | 0 |
-   | b | 1011 | 1 | 1 | 0 | 0 | 0 | 0 | 0 |
-   | C | 1100 | 0 | 1 | 1 | 0 | 0 | 0 | 1 |
-   | d | 1101 | 1 | 0 | 0 | 0 | 0 | 1 | 0 |
-   | E | 1110 | 0 | 1 | 1 | 0 | 0 | 0 | 0 |
-   | F | 1111 | 0 | 1 | 1 | 1 | 0 | 0 | 0 |
+------------------------------------------------------------
+-- Entity declaration for testbench
+------------------------------------------------------------
+entity tb_hex_7seg is
+    -- Entity of testbench is always empty
+end entity tb_hex_7seg;
 
-![7-segment display](images/segment2.png)
+------------------------------------------------------------
+-- Architecture body for testbench
+------------------------------------------------------------
+architecture testbench of tb_hex_7seg is
 
-![7-segment display schematic](images/segment1.png)
+    -- Local signals
+    signal s_hex  : std_logic_vector(4 - 1 downto 0);
+    signal s_seg  : std_logic_vector(7 - 1 downto 0);
 
-## Seven-segment display decoder
+begin
+    -- Connecting testbench signals with decoder entity
+    -- (Unit Under Test)
+    uut_hex_7seg : entity work.hex_7seg
+        port map(
+            hex_i => s_hex,
+            seg_o => s_seg
+        );
 
-1. Listing of VHDL stimulus process from testbench file (`tb_hex_7seg.vhd`) with asserts. Verify all input combinations. Always use syntax highlighting, meaningful comments, and follow VHDL guidelines:
-
-```vhdl
-     p_stimulus : process
+    --------------------------------------------------------
+    -- Data generation process
+    --------------------------------------------------------
+    p_stimulus : process
     begin
         report "Stimulus process started" severity note;
 
@@ -101,33 +111,5 @@
         report "Stimulus process finished" severity note;
         wait;
     end process p_stimulus;
-```
 
-2. Screenshot with simulated time waveforms. Always display all inputs and outputs (display the inputs at the top of the image, the outputs below them) at the appropriate time scale!
-
-   ![Waveforms](images/waveforms.png)
-
-## LED(7:4) indicators
-
-1. Listing of LEDs(7:4) part of VHDL architecture from source file `top.vhd`. Try to write logic functions as simple as possible. Always use syntax highlighting, meaningful comments, and follow VHDL guidelines:
-
-   ```vhdl
-   --------------------------------------------------------------------
-   -- Experiments on your own: LED(7:4) indicators
-
-   -- Turn LED(4) on if input value is equal to 0, ie "0000"
-   LED(4) <= '0' when (SW = "0000") else '1';
-
-    -- Turn LED(5) on if input value is greater than "1001", ie 10, 11, 12, ...
-    LED(5) <= '0' when (SW > "1001") else '1';
-
-    -- Turn LED(6) on if input value is odd, ie 1, 3, 5, ...
-    LED(6) <= '0' when (SW(0) > '1') else '1';
-
-    -- Turn LED(7) on if input value is a power of two, ie 1, 2, 4, or 8
-    LED(7) <= '0' when (SW = "0001") else
-              '0' when (SW = "0010") else
-              '0' when (SW = "0100") else  
-              '0' when (SW = "1000") else  
-              '1';
-   ```
+end architecture testbench;
